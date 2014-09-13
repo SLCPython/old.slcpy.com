fe6a72adcd710bdb393613b9b63ed7490be21dce
astrodsg_laptop <gregersen.dylan@gmail.com>
2014-09-13 12:31:41 -0600

    added logic to the manage.py so that developers may have their own local_settings.py but it's not necessary

--- manage.py
+++ manage.py
@@ -3,8 +3,18 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "slcpy.settings.local")
 
+    # decide which settings to use
+    base_dir = os.path.dirname(__file__)
+    local_settings_module = "slcpy/settings/local_settings.py")    
+    if os.path.isfile(os.path.join(base_dir,"apps",local_settings_module)):
+        django_settings_module = local_settings_module
+    else:
+        django_settings_module = "slcpy.settings.local"
+
+    # set the local settins
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE",django_settings_module)
+
+    # execute from command line
     from django.core.management import execute_from_command_line
-
     execute_from_command_line(sys.argv)
