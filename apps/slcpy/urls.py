1f79ad9b7aec167f7cc468a31d78053839045354
astrodsg_laptop <gregersen.dylan@gmail.com>
2014-09-16 23:57:22 -0600

    added meetup as a required app. Included methods for viewing

--- apps/slcpy/urls.py
+++ apps/slcpy/urls.py
@@ -9,12 +9,19 @@ urlpatterns = patterns('',
     # ----------------------- Home
     url(r'^$', 'slcpy.views.home.main_view', name='home'),
 
+    # add frame for meetup home
+    # url(r'^meetup-home$', 'slcpy.views.home.meetup_home_view', name="meetup-home"),
+
     # ----------------------- profile
-    url(r'^login$', 'profiles.views.home.login_view', name='login'),    
-    url(r'^register$', 'profiles.views.home.register_view', name='register'),          
-    url(r'^profiles/', include('profiles.urls', namespace='profiles')),
+    # TODO: remove profile all-togehter, I don't think we need it    
+    #url(r'^login$', 'profiles.views.home.login_view', name='login'),    
+    #url(r'^register$', 'profiles.views.home.register_view', name='register'),          
+    #url(r'^profiles/', include('profiles.urls', namespace='profiles')),
     # TODO:  url(r'^logout$', 'profiles.views.home.logout_view', name='logout'),        
 
+    # ----------------------- Events
+    url(r'^events$','meetup.views.view_upcoming_past_events',name='events'),
+    
     # ----------------------- API urls
     #url(r'^api/', include(tagged_post.urls)),
 
